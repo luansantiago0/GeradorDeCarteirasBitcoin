10016fb8835f82ad7c0d4b37e088350fab28047a
luansantiago0 <luansantiago0200@gmail.com>
2023-07-24 18:36:46 -0300

    Gerador de carteiras Bitcoin

---
+++ src/createWallet.js
@@ -0,0 +1,33 @@
+// Importando as dependências.
+const bip32 = require("bip32");
+const bip39 = require("bip39");
+const bitcoin = require("bitcoinjs-lib");
+
+// Definir a rede.
+// Bitcoin rede principal = mainnet
+// Testnet rede de teste = testnet
+const network = bitcoin.networks.testnet;
+
+//Derivação de carteiras Hieraquica deterministica.
+const path = `m/49'/1'/0'/0`;
+
+// Criano o mnemonic para a seed (Palavras de senha)
+let mnemonic = bip39.generateMnemonic();
+const seed = bip39.mnemonicToSeedSync(mnemonic);
+
+// Criando a raiz da carteira HD
+let root = bip32.fromSeed(seed, network);
+
+// Criando uma conta de par pvt-pub keys
+let account = root.derivePath(path);
+let node = account.derive(0).derive(0);
+
+let btcAddress = bitcoin.payments.p2pkh({
+  pubkey: node.publicKey,
+  network: network,
+}).address;
+
+console.log("Carteira gerada");
+console.log("Endereço: ", btcAddress);
+console.log("Chave Privada: ", node.toWIF());
+console.log("Seed", mnemonic);
